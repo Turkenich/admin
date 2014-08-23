00c5b22e2f22bea48484940dd048976904104819
Guy Sopher <guysopher@gmail.com>
2014-08-23 21:26:19 +0300

    add version log

--- app/scripts/controllers/root.js
+++ app/scripts/controllers/root.js
@@ -2,6 +2,9 @@
 
 angular.module('adminApp')
     .controller('RootCtrl', ['$scope', '$sce', function ($scope, $sce) {
+
+        console.log('VERSION: ' + '1.0');
+
         $scope.trustUrl = function(url){
             return $sce.trustAsResourceUrl(url);
         }
