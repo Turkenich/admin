98c6e9b2e0a73e6c7f3b2125610c0c7ef8366c51
Guy Sopher <guysopher@gmail.com>
2015-05-30 23:38:45 +0300

    grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -245,7 +245,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif}',
+          src: '{,*/}*.{png,jpg,jpeg,gif,swf}',
           dest: '<%= yeoman.dist %>/images'
         }]
       }
@@ -313,7 +313,7 @@ module.exports = function (grunt) {
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
-            'images/{,*/}*.{webp}',
+            'images/{,*/}*.{webp,swf}',
             'fonts/*'
           ]
         }, {
